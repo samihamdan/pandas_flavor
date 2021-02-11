c40c833ef4515ac7f15f43901778f328ab6a931a
samihamdan <sami.hamdan@hhu.de>
2021-02-11 21:33:34 +0100

    Add groubpy methods to __init__

--- pandas_flavor/__init__.py
+++ pandas_flavor/__init__.py
@@ -1,7 +1,8 @@
-from .register import (register_series_method,
+from .register import (register_groupby_method, register_series_method,
                        register_series_accessor,
                        register_dataframe_method,
-                       register_dataframe_accessor)
-from .xarray import (register_xarray_dataarray_method, 
+                       register_dataframe_accessor,
+                       register_groupby_accessor,
+                       register_groupby_method)
+from .xarray import (register_xarray_dataarray_method,
                      register_xarray_dataset_method)
-
