a8c5d8c1d9283e0165684dbb17c087e007cafcff
samihamdan <sami.hamdan@hhu.de>
2021-02-11 21:55:43 +0100

    Bugfix logic of importing version dependent

--- pandas_flavor/pandas_internals.py
+++ pandas_flavor/pandas_internals.py
@@ -4,9 +4,8 @@ try:
     from pandas.api.extensions import (register_dataframe_accessor,
                                        register_series_accessor)
 
-    # Register groupby >= 0.23
     try:
-        # >= 1.0
+        # Define groupby register decorater from pandas >= 1.0
         from pandas.util._decorators import doc
         from pandas.core.accessor import _register_accessor
 
@@ -17,22 +16,22 @@ try:
             return _register_accessor(name, DataFrameGroupBy)
 
     except ImportError:
-        try:
-            # 0.23 <= Pandas > 1.0
-            from pandas.core.accessor import _doc, _register_accessor
-            from pandas.util._decorators import Appender
+        # Define groupby register decorater from pandas >= 0.23
+        # and before 1.0
+        from pandas.core.accessor import _doc, _register_accessor
+        from pandas.util._decorators import Appender
 
-            @Appender(
-                _doc
-                % dict(
-                    klass="DataFrame", others=(
-                        "register_groupby_accessor, register_index_accessor")
-                )
+        @Appender(
+            _doc
+            % dict(
+                klass="DataFrame", others=(
+                    "register_groupby_accessor, register_index_accessor")
             )
-            def register_groupby_accessor(name):
-                """Register custom accessor on DataFrameGroupBy."""
+        )
+        def register_groupby_accessor(name):
+            """Register custom accessor on DataFrameGroupBy."""
 
-                return _register_accessor(name, DataFrameGroupBy)
+            return _register_accessor(name, DataFrameGroupBy)
 
 except ImportError:
     from pandas import DataFrame, Series
@@ -50,7 +49,8 @@ except ImportError:
             self.name = name
 
         def __call__(self, accessor):
-            setattr(DataFrame, self.name, AccessorProperty(accessor, accessor))
+            setattr(DataFrame, self.name,
+                    AccessorProperty(accessor, accessor))
 
     class register_series_accessor(object):
         """Register custom accessor on Series."""
@@ -59,7 +59,8 @@ except ImportError:
             self.name = name
 
         def __call__(self, accessor):
-            setattr(Series, self.name, AccessorProperty(accessor, accessor))
+            setattr(Series, self.name,
+                    AccessorProperty(accessor, accessor))
 
     class register_groupby_accessor(object):
         """Register custom accessor on DataFrameGroupBy."""
