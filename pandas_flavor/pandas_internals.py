bd84a2f419b20dfa01bc5eafe9125763857d98db
samihamdan <sami.hamdan@hhu.de>
2021-02-11 21:22:21 +0100

    Add register_groupby_accessor

--- pandas_flavor/pandas_internals.py
+++ pandas_flavor/pandas_internals.py
@@ -1,7 +1,39 @@
+from pandas.core.groupby.generic import DataFrameGroupBy
 try:
     # Import register decorators from pandas >= 0.23
     from pandas.api.extensions import (register_dataframe_accessor,
                                        register_series_accessor)
+
+    # Register groupby >= 0.23
+    try:
+        # >= 1.0
+        from pandas.util._decorators import doc
+        from pandas.core.accessor import _register_accessor
+
+        @doc(_register_accessor, klass="DataFrameGroupBy")
+        def register_groupby_accessor(name):
+            """Register custom accessor on DataFrameGroupBy."""
+
+            return _register_accessor(name, DataFrameGroupBy)
+
+    except ImportError:
+        try:
+            # 0.23 <= Pandas > 1.0
+            from pandas.core.accessor import _doc, _register_accessor
+            from pandas.util._decorators import Appender
+
+            @Appender(
+                _doc
+                % dict(
+                    klass="DataFrame", others=(
+                        "register_groupby_accessor, register_index_accessor")
+                )
+            )
+            def register_groupby_accessor(name):
+                """Register custom accessor on DataFrameGroupBy."""
+
+                return _register_accessor(name, DataFrameGroupBy)
+
 except ImportError:
     from pandas import DataFrame, Series
 
@@ -13,6 +45,7 @@ except ImportError:
     # Define register decorators for pandas < 0.23
     class register_dataframe_accessor(object):
         """Register custom accessor on DataFrame."""
+
         def __init__(self, name):
             self.name = name
 
@@ -21,8 +54,19 @@ except ImportError:
 
     class register_series_accessor(object):
         """Register custom accessor on Series."""
+
         def __init__(self, name):
             self.name = name
 
         def __call__(self, accessor):
             setattr(Series, self.name, AccessorProperty(accessor, accessor))
+
+    class register_groupby_accessor(object):
+        """Register custom accessor on DataFrameGroupBy."""
+
+        def __init__(self, name):
+            self.name = name
+
+        def __call__(self, accessor):
+            setattr(DataFrameGroupBy, self.name,
+                    AccessorProperty(accessor, accessor))
